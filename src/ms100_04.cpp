24a927d706aa27e0f50ea3849f27c6ccd9998dd4
stevegocoding <stevegocoding@gmail.com>
2012-10-05 03:00:17 -0400

    Added ms100_04, but not finished!

--- src/ms100_04.cpp
+++ src/ms100_04.cpp
@@ -7,8 +7,6 @@
 
 #define SAFE_DELETE(p) if (p) {delete (p); (p) = NULL; }
 
-using namespace std;
-
 struct bstree_node
 {
 	bstree_node(int _val)
@@ -16,7 +14,7 @@ struct bstree_node
 		, left(NULL)
 		, right(NULL)
 	{}
-	
+
 	~bstree_node() 
 	{
 		left = right = NULL; 
@@ -62,39 +60,15 @@ void destroy_bstree(bstree_node *& node_p)
 	}
 
 	destroy_bstree(node_p->left); 
-	
+
 	destroy_bstree(node_p->right); 
 
 	SAFE_DELETE(node_p); 
 }
 
-void breadth_first_traversal(bstree_node *root)
-{
-	if (!root) 
-		return; 
-	
-	deque<bstree_node*> nodes_deq; 
-	
-	// Insert the root node to the tail 
-	nodes_deq.push_back(root); 
-	
-	while(nodes_deq.size())
-	{
-		bstree_node *node_p = nodes_deq.front(); 
-		nodes_deq.pop_front();
-		
-		// Print the node
-		cout << node_p->val << " "; 
-		if (node_p->left)
-			nodes_deq.push_back(node_p->left); 
-		if (node_p->right)
-			nodes_deq.push_back(node_p->right); 
-	}
-}
-
 int main(int argc, char **argv)
 {
-	ifstream ifs("ms100_16.in");
+	ifstream ifs("ms100_04.in");
 	vector<int> data_vec;
 	copy(istream_iterator<int>(ifs), istream_iterator<int>(), back_inserter(data_vec));
 	ifs.close();
@@ -104,10 +78,8 @@ int main(int argc, char **argv)
 	// Create the bst
 	create_bstree(root, data_vec);
 	
-	breadth_first_traversal(root); 
-	
 	// Destroy the bst
 	destroy_bstree(root);
-	
+
 	return 0; 
 }
