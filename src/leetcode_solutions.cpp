b8c607ca43b0ff75990d6b97d1c94bc448d3ed02
stevegocoding <stevegocoding@gmail.com>
2013-07-28 14:58:25 -0700

    palindrome partition II passed all tests! based on the previous check-in, use another DP for palindrome testing.

--- src/leetcode_solutions.cpp
+++ src/leetcode_solutions.cpp
@@ -69,18 +69,90 @@ public:
 		string s = "aab"; 
 		vector<vector<string> > result = partition(s);
 		
-		for (int i = 0; i < result.size(); ++i)
+		for (int i = 0; i < (int)result.size(); ++i)
 		{
-			for (int j = 0; j <result[i].size(); ++j)
+			for (int j = 0; j < (int)result[i].size(); ++j)
 				std::cout << result[i][j] << std::endl; 
 		}
 	}
 	
 };
 
+
+
+class PalindromePartitioningII : public c_leetcode_solution<PalindromePartitioningII>
+{
+	/*
+	 *	dp[i] 表示以i个字符为结尾的字符串的最少切割数目 状态转移方程为：
+		dp[i] = min(dp[k-1] + 1)		(for any k in [0, i], if str[k...i] is palindrome)
+				0						(if str[0, i] is palindrome)
+	 */
+
+public:
+
+	vector<int> dp;
+	vector<vector<bool> > palin_dp;
+	
+	int minCut(string s)
+	{
+		dp.resize(s.length());
+		std::fill(dp.begin(), dp.end(), 0); 
+		
+		palin_dp.resize(s.length()); 
+		for (int i = 0; i < s.length(); ++i)
+			palin_dp[i] = vector<bool>(s.length(), false); 
+		
+		for (int i = 1; i < (int)s.length(); ++i)
+		{
+			if (is_palindrome(s, 0, i)) 
+			{
+				dp[i] = 0;
+			}
+			else
+			{
+				int min = -1;
+				for (int j = 1; j <= i; ++j)
+				{
+					//if (is_palindrome(s, j, i))
+					if (s[i] == s[j] && ((i-j) < 2 || palin_dp[j+1][i-1]))
+					{
+						palin_dp[j][i] = true;
+						if (min == -1 || (1+dp[j-1]) < min)
+							min = dp[j-1]+1; 
+					}
+				}
+				dp[i] = min; 
+			}
+		}
+
+		return dp[s.length()-1]; 
+	}
+
+	bool is_palindrome(const string& s, int start, int end)
+	{
+		while (start < end)
+		{
+			if (s[start++] != s[end--])
+				return false;
+		}
+		return true; 
+	}
+
+	void test()
+	{
+		string s = "leet"; 
+		
+		int result = minCut(s);
+		cout << result << endl; 
+	}
+	
+};
+
 int main(int argc, char **argv)
 {
-	c_leetcode_solution<PalindromePartitioning>::run_test();
+	// c_leetcode_solution<PalindromePartitioning>::run_test();
 	
+	c_leetcode_solution<PalindromePartitioningII>::run_test();
+
 	return 0;
 }
